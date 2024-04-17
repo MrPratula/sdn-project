d6c21273f75226d238629712f994abb7d55c04ac
MrPratula <lorenzo_.prada@hotmail.it>
2024-04-17 17:10:23 +0200

    update

--- project/test1.py
+++ project/test1.py
@@ -13,6 +13,7 @@ def should_pass(pkt):
 
     if pkt.get_protocol(ethernet.ethernet).ethertype == ether_types.ETH_TYPE_IPV6:
         print("discarded IPv6 packet")
+        print(pkt)
         return False
 
     elif pkt.get_protocol(ethernet.ethernet).ethertype == ether_types.ETH_TYPE_ARP:
