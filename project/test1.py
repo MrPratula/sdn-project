49dc7231726efe3122f9d578c98952c1d59039a0
MrPratula <lorenzo_.prada@hotmail.it>
2024-04-17 17:30:36 +0200

    update

--- project/test1.py
+++ project/test1.py
@@ -13,9 +13,9 @@ def should_pass(pkt):
 
     if pkt.get_protocol(ethernet.ethernet).ethertype == ether_types.ETH_TYPE_IPV6:
 
-        print(pkt.protocols)
+        print(pkt.get_protocol(ipv6.ipv6))
 
-        if pkt.protocols.nxt == inet.IPPROTO_ICMPV6:
+        if pkt.get_protocol(ipv6.ipv6) == inet.IPPROTO_ICMPV6:
             print("ICMPv6 packet")
             return True
         else:
