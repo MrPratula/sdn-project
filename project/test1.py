9174e5c8d8ef4d9bed9895c2d0c61cd140044251
MrPratula <lorenzo_.prada@hotmail.it>
2024-05-02 18:17:57 +0200

    update

--- project/test1.py
+++ project/test1.py
@@ -45,7 +45,9 @@ def check_pkt(pkt):
 
                 else:
 
-                    if pkt.data.contains("HTTP/1.1") or pkt.data.contains("HTTP/1.0") or pkt.data.contains("HTTP/2.0"):
+                    pkt_str = str(pkt.data)
+
+                    if "HTTP/1.0" in pkt_str or "HTTP/1.1" in pkt_str or "HTTP/2.0" in pkt_str:
 
                         print("HTTP PACKET")
 
